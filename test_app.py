c73e64904e3e5523e12ca1995d04fcc924244faf
MaxLopezSalgado <maxlopezsalgado@gmail.com>
2023-08-14 14:57:45 +0200

    test performed: 8 passed, 3 failed

--- test_app.py
+++ test_app.py
@@ -13,6 +13,7 @@ client = TestClient(app)
 mongo_client = MongoClient(URI)
 db = mongo_client['courses']
 
+
 # Test the Get Courses List Endpoint
 def test_get_courses_no_params():
     response = client.get("/courses")
@@ -63,6 +64,7 @@ def test_get_courses_filter_by_domain_and_sort_by_date():
     assert all([c['domain'][0] == 'mathematics' for c in courses])
     assert sorted(courses, key=lambda x: x['date'], reverse=True) == courses
 
+
 # Test the Get Single Course Info Endpoint
 def test_get_course_by_id_exists():
     response = client.get("/courses/6431137ab5da949e5978a281")
@@ -83,6 +85,7 @@ def test_get_course_by_id_not_exists():
     assert response.status_code == 404
     assert response.json() == {'detail': 'Course not found'}
 
+
 # Test the Get Course Chapter Info Endpoint
 def test_get_chapter_info():
     response = client.get("/courses/6431137ab5da949e5978a281/1")
@@ -96,3 +99,29 @@ def test_get_chapter_info_not_exists():
     response = client.get("/courses/6431137ab5da949e5978a281/990")
     assert response.status_code == 404
     assert response.json() == {'detail': 'Chapter not found'}
+
+
+# Test the Post Course Rating Endpoint
+def test_rate_chapter():
+    course_id = "6431137ab5da949e5978a281"
+    chapter_id = "1"
+    rating = 1
+
+    response = client.post(f"/courses/{course_id}/{chapter_id}?rating={rating}")
+
+    assert response.status_code == 200
+
+    # Check if the response body has the expected structure
+    assert "name" in response.json()
+    assert "rating" in response.json()
+    assert "total" in response.json()["rating"]
+    assert "count" in response.json()["rating"]
+
+    assert response.json()["rating"]["total"] > 0
+    assert response.json()["rating"]["count"] > 0
+     
+def test_rate_chapter_not_exists():
+    response = client.post("/courses/6431137ab5da949e5978a281/990/rate", json={"rating": 1})
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Not Found'}
+    
